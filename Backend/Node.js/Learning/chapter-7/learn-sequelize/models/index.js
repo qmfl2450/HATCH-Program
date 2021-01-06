b14048db968d9fce6a352e5f0202230718e7981e
sumi <qmfl2450@naver.com>
2021-01-06 23:52:00 +0900

    node.js chapter7 mysql coding

---
+++ Backend/Node.js/Learning/chapter-7/learn-sequelize/models/index.js
@@ -0,0 +1,66 @@
+// 'use strict';
+
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const basename = path.basename(__filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.json')[env];
+// const db = {};
+
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+// fs
+//   .readdirSync(__dirname)
+//   .filter(file => {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
+
+
+
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+db.sequelize = sequelize;
+
+// 여기서부터 모델 연결
+//db 객체에 User와 Comment 모델을 담아둠
+db.User = User;
+db.Comment = Comment;
+
+// 각 모델의 static.init 메서드를 호출
+// init이 실행되어야 테이블이 모텔로 연결됨
+User.init(sequelize);
+Comment.init(sequelize);
+
+// 다른 테이블과의 관계를 연결하는 메서드 associate
+User.associate(db);
+Comment.associate(db);
+
+module.exports = db;
