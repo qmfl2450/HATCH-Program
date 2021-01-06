9051c1fa19028ff326984a18f3392e8b6475fddb
sumi <qmfl2450@naver.com>
2021-01-07 02:37:27 +0900

    node.js chapter-7 complete

--- Backend/Node.js/Learning/chapter-7/learn-sequelize/app.js
+++ Backend/Node.js/Learning/chapter-7/learn-sequelize/app.js
@@ -1,45 +1,97 @@
+// const express = require('express');
+// const path = require('path');
+// const morgan = require('morgan');
+// const nunjucks = require('nunjucks');
+
+// const {sequelize} = require('./models');
+
+// const app = express();
+// app.set('port', process.env.PORT || 3001);
+// app.set('view engine', 'html');
+// nunjucks.configure('views', {
+//     express: app,
+//     watch: true,
+// });
+// // 서버 실행 시 MySQL과 연동
+// sequelize.sync({force: false})
+//     .then(() => {
+//         console.log('데이터베이스 연결 성공');
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     });
+
+// app.use(morgan('dev'));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+
+// app.use(express.urlencoded({extended: false}));
+
+// app.use((req, res, next) => {
+//     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+//     error.status = 404;
+//     next(error);
+// });
+
+// app.use((err, req, res, next) => {
+//     res.locals.message = err.message;
+//     res.lacals.error = process.env.NODE_ENV !== 'production' ? err : {};
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
+
+// app.listen(app.get('port'), () => {
+//     console.log(app.get('port'), '번 포트에서 대기 중')
+// })
+
+// 7.6.5 장
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 
-const {sequelize} = require('./models');
+const { sequelize } = require('./models');
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
 
 const app = express();
 app.set('port', process.env.PORT || 3001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
-    express: app,
-    watch: true,
+  express: app,
+  watch: true,
 });
-// 서버 실행 시 MySQL과 연동
-sequelize.sync({force: false})
-    .then(() => {
-        console.log('데이터베이스 연결 성공');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended: false}));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.lacals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중')
-})
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
