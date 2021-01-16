b3182238d42c0c9801ea68238f7e0ade6e886d9f
sumi <qmfl2450@naver.com>
2021-01-16 20:42:48 +0900

    react 3.1 finished

--- Frontend/REACT/nomadCoder/movie_app/src/App.js
+++ Frontend/REACT/nomadCoder/movie_app/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-function App() {
-  return <div className="App">
-    <h1> Hello!! </h1>
-  </div>;
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
