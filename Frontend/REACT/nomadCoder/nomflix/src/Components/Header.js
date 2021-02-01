e74725dd2b70ac2765d27d5105f39b97484bfb3b
sumi <qmfl2450@naver.com>
2021-02-01 23:51:10 +0900

    nomflix 3.4 ing

--- Frontend/REACT/nomadCoder/nomflix/src/Components/Header.js
+++ Frontend/REACT/nomadCoder/nomflix/src/Components/Header.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
 
-const List = styled.ul`
-    display: flex;
-    &:hover {
-    background-color: blue;
-    }
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
-export default () => (
-    <header>
-        <List>
-            <li>
-                <a href='/'>Movies</a>
-            </li>
-            <li>
-                <a href='/tv'>TV</a>
-            </li>
-            <li>
-                <a href='/search'>Search</a>
-            </li>
-        </List>
-    </header>
-);
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  border-bottom: 5px solid ${props => props.current ? "#3498db" : "transparent"};
+`;
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default withRouter(props => (
+    <Header>
+      <List>
+        <Item current={false}>
+          <SLink to="/">Movies</SLink>
+        </Item>
+        <Item current={true}>
+          <SLink to="/tv">TV</SLink>
+        </Item>
+        <Item current={false}>
+          <SLink to="/search">Search</SLink>
+        </Item>
+      </List>
+    </Header>
+  ));
--- Frontend/REACT/nomadCoder/nomflix/src/Components/Router.js
+++ Frontend/REACT/nomadCoder/nomflix/src/Components/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import Header from './Header';
 import Home from '../Routes/Home';
 import TV from '../Routes/TV';
 import Search from '../Routes/Search';
 
 export default () => (
     <Router>
+        <Header />
         <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/tv' component={TV} />
