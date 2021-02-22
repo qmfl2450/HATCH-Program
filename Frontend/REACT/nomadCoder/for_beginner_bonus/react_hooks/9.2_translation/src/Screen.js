f1f35661aaafd4fce0cc29ff71b5d2462ef4e443
sumi <qmfl2450@naver.com>
2021-02-22 12:06:41 +0900

    9.4 ing

--- Frontend/REACT/nomadCoder/for_beginner_bonus/react_hooks/9.2_translation/src/Screen.js
+++ Frontend/REACT/nomadCoder/for_beginner_bonus/react_hooks/9.2_translation/src/Screen.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSetLang } from "./context";
 
-export default () => (
-  <>
-    <h1>Hello!</h1>
-    <button>Translate</button>
-  </>
-);
+export default () => {
+  const { setLang } = useSetLang();
+  return (
+    <>
+      <h1>Hello!</h1>
+      <button onClick={setLang("es")}>Translate</button>
+    </>
+  );
+};
--- Frontend/REACT/nomadCoder/for_beginner_bonus/react_hooks/9.2_translation/src/context.js
+++ Frontend/REACT/nomadCoder/for_beginner_bonus/react_hooks/9.2_translation/src/context.js
@@ -4,14 +4,15 @@ const LangContext = createContext();
 
 const Lang = ({ defaultLang, children, translations }) => {
   const [Lang, setLang] = useState(defaultLang);
+  console.log(Lang);
   return (
     <LangContext.Provider value={{ setLang }}>{children}</LangContext.Provider>
   );
 };
 
-export const changeLang = (Lang) => {
+export const useSetLang = (lang) => {
   const { setLang } = useContext(LangContext);
-  setLang(lang);
+  return setLang;
 };
 
 export default Lang;
